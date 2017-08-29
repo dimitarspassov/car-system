bc6158a12e023aba29e91cae025c19a1ec65cbd6
dimitarspassov <dimitar.spassov7@gmail.com>
2017-08-29 21:40:28 +0300

    Refactored html for the home page & minor fixes
    The homepage is now with a different design. The fixes: Pagination is available only if there are cars. The mileage input is now with type number. Profile page now provides links for the cars' details pages.

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -7,10 +7,11 @@ import {LoginComponent} from './login.component';
 import {ProfileComponent} from './profile.component';
 import {CommonModule} from '@angular/common';
 import {CarsActions} from '../store/cars/cars.actions';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   declarations: [RegisterComponent, LoginComponent, ProfileComponent],
   providers: [UsersService, UsersActions, CarsActions],
   exports: []
